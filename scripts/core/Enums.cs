2520d592ee637b02ef700230e8df3c770f4bfacd
Saman <teamtvrt@gmail.com>
2025-03-28 14:02:35 -0400

    Part 3 Save

--- scripts/core/Enums.cs
+++ scripts/core/Enums.cs
@@ -7,4 +7,20 @@ namespace Game.Core
         WARNING,
         ERROR
     }
+
+    public enum ECharacterAnimation
+    {
+        idle_down,
+        idle_up,
+        idle_left,
+        idle_right,
+        turn_down,
+        turn_up,
+        turn_left,
+        turn_right,
+        walk_down,
+        walk_up,
+        walk_left,
+        walk_right,
+    }
 }
