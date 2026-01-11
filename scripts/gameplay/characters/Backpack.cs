b0506eeb4431439012deb4bc1107435b78557c79
Baba <teamtvrt@gmail.com>
2026-01-11 13:40:53 -0500

    Adding to the backpack

--- scripts/gameplay/characters/Backpack.cs
+++ scripts/gameplay/characters/Backpack.cs
@@ -1,7 +1,32 @@
+using Game.Core;
 using Godot;
-using System;
+
+namespace Game.Gameplay;
 
 public partial class Backpack : Node
 {
+    [ExportCategory("Components")]
+    [Export]
+    public Node BackpackOwner;
+
+    [Export]
+    public Node Party;
+
+    [Export]
+    public Node Items;
+
+    public override void _Ready()
+    {
+        Party ??= GetNode("Party");
+        Items ??= GetNode("Items");
+    }
+
+    public void AddPokemonToParty(PokemonResource pokemonResource, int level, bool shiny, PokemonMetData pokemonMetData, PokemonNature pokemonNature)
+    {
+        Logger.Info($"Adding pokemon to {BackpackOwner}'s party: {pokemonResource.Name}");
+        PokemonInstance pokemon = new();
+        Party.AddChild(pokemon);
+        pokemon.Initialize(pokemonResource, level, shiny, pokemonMetData, pokemonNature);
+    }
 
 }
