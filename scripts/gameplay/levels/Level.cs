84385f6f6a92f39d41a96b73485fdb9f2b7cdda7
Baba <teamtvrt@gmail.com>
2026-01-15 22:37:51 -0500

    save

--- scripts/gameplay/levels/Level.cs
+++ scripts/gameplay/levels/Level.cs
@@ -51,7 +51,7 @@ public partial class Level : Node2D
 			SetupGrid();
 		}
 
-		var debugLayer = GetNode<LevelDebugger>("D	ebugLayer");
+		var debugLayer = GetNode<LevelDebugger>("DebugLayer");
 
 		if (debugLayer.DebugOn != DebugLayerOn)
 			debugLayer.DebugOn = DebugLayerOn;
